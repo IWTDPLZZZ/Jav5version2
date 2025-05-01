f3222487134bd12963f4b64e1840e8ea4433122d
IWTDPLZZZ <kavelichmatvey@gmail.com>
2025-05-01 17:07:31 +0300

    Update Swagger

--- src/main/java/orf/demo/config/SwaggerConfig.java
+++ src/main/java/orf/demo/config/SwaggerConfig.java
@@ -1,20 +1,18 @@
-//package orf.demo.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//@Configuration
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("orf.demo.controller"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//}
+package orf.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spell Check API")
+                        .version("1.0")
+                        .description("API для проверки орфографии и управления категориями"));
+    }
+}
